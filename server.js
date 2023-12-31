8e46134856c44712b7cfc30f39a5f0bfc1da907f
Mayank Anand <mayank.96anand@gmail.com>
2023-12-31 14:43:27 +0530

    updated Index file to handle error with try catch block

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT   || 3000;
 
 
 //app.use(express.static('public')); // Serve static files from the 'public' directory
